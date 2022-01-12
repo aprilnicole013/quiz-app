3ff89464104732256f2c8a61a147c267c8b7deff
aprilnicole013 <aprilnicole013@gmail.com>
2022-01-12 18:34:40 -0500

    Questions rendering in DOM

--- script.js
+++ script.js
@@ -90,4 +90,33 @@ const quizData = [
         correct: 'a',
         explanation: 'reverse() − Reverses the order of the elements of an array.'
     }
-]
+];
+
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text') 
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question
+
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+
+    
+}
+
+submitBtn.addEventListener('click', () => {
+    currentQuiz ++;
+    loadQuiz()
+})
